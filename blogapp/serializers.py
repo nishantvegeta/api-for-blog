0fee4614faae27f3e9007630a125bf883725f1ac
nishantvegeta <nishantkumpakha03@gmail.com>
2024-05-28 14:38:43 +0545

    new commit

--- blogapp/serializers.py
+++ blogapp/serializers.py
@@ -4,5 +4,5 @@ from .models import Blog
 class BlogSerializers(serializers.ModelSerializer):
     class Meta:
         model = Blog
-        fields = '__all__'
+        fields = ['id', 'title', 'description', 'category']
         
